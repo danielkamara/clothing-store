662e6d5b8c2bbc77b28fc2c6ed38817bb32bc0b4
daniel <danlikestech@gmail.com>
2022-02-24 19:19:01 -0600

    finished working on the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,98 @@
+import styled from "styled-components";
+
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <FooterContainer className="main-footer">
+      <div className="footer-middle">
+        <div className="container">
+          <div className="row">
+            {/* Column 1 */}
+            <div className="col-md-3 col-sm6">
+              <h4>Contact Us</h4>
+              <ul className="list-unstyled">
+                <li>Dallas, TX</li>
+                <li>239 Main Street</li>
+                <li>phone: 112-333-4444</li>
+              </ul>
+            </div>
+            {/* Column 2 */}
+            <div className="col-md-3 col-sm6">
+              <h4>Lorem Ipsum</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="/">Lorem Ipsum </a>{" "}
+                </li>
+                <li>
+                  <a href="/">Lorem Ipsum </a>{" "}
+                </li>
+                <li>
+                  <a href="/">Lorem Ipsum </a>{" "}
+                </li>
+              </ul>
+            </div>
+            {/* Column 3 */}
+            <div className="col-md-3 col-sm6">
+              <h4>Lorem Ipsum</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="/">Lorem Ipsum </a>{" "}
+                </li>
+                <li>
+                  <a href="/">Lorem Ipsum </a>{" "}
+                </li>
+                <li>
+                  <a href="/">Lorem Ipsum </a>{" "}
+                </li>
+              </ul>
+            </div>
+            {/* Column 4 */}
+            <div className="col-md-3 col-sm6">
+              <h4>Lorem Ipsum</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="/">Lorem Ipsum </a>{" "}
+                </li>
+                <li>
+                  <a href="/">Lorem Ipsum </a>{" "}
+                </li>
+                <li>
+                  <a href="/">Lorem Ipsum </a>{" "}
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* Footer Bottom */}
+          <div className="footer-bottom">
+            <p className="text-xs-center">
+              &copy;Copyright {new Date().getFullYear()} The Fit - All Rights
+              Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
+
+const FooterContainer = styled.footer`
+  .footer-middle {
+    background: var(--mainDark);
+    padding-top: 3rem;
+    color: var(--mainWhite);
+  }
+
+  .footer-bottom {
+    padding-top: 3rem;
+    padding-bottom: 2rem;
+  }
+
+  ul li a {
+    color: var(--mainGray);
+  }
+
+  ul li a:hover {
+    color: var(--mainLightGray);
+  }
+`;
