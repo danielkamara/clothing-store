781e3870e54725580520b977c91acaba11c1ae3e
daniel <danlikestech@gmail.com>
2022-02-28 17:00:16 -0600

    added Images to the card component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -9,13 +9,10 @@ const ProductList = ({ fakeStore }) => {
           return (
             <>
               <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Img variant="top" src={product.image} />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text>${product.price}</Card.Text>
                   <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
               </Card>
